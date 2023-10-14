19ffbf41858e0ac0d08c9b2ae281d9b913871891
Ritikraja13 <115801193+rajasingh05@users.noreply.github.com>
2023-10-14 14:59:49 +0000

    arrays

---
+++ JAVA/practice3.java
@@ -0,0 +1,33 @@
+// Q3. Count the number of element strictly greater than value x.
+
+import java.util.*;
+public class practice3 {
+
+    static int greaterthan(int[] arr, int x) {
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > x) {
+                count++;
+            }
+        }
+        return count;
+    } 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int [] arr = new int[n];
+        System.out.println("Enter " + n + " elements: ");
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter X: ");
+        int x = sc.nextInt();
+
+        System.out.println("COUNT OF X: " + greaterthan(arr, x));
+
+        sc.close();
+    }
+}
